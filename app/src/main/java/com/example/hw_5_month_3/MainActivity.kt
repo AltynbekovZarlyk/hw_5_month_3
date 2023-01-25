353d6ba10233f5f97c16dad63b19b6b09ca03f43
Altynbekovzarlyk <altynbekov.zarlyk7@gmail.com>
2023-01-25 13:00:23 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw_5_month_3/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.hw_5_month_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hw_5_month_3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null)
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, OperationFragment()).commit()
+    }
+}
